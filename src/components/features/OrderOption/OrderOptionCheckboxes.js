3e337bbeefd2b29d211a29e6d5931f26dc7e7c01
Mariusz Jabłoński <amidek321@gmail.com>
2019-11-22 21:35:56 +0100

    dodanie opcji zamówienia

--- src/components/features/OrderOption/OrderOptionCheckboxes.js
+++ src/components/features/OrderOption/OrderOptionCheckboxes.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
 
-const OrderOptionCheckboxes = () => {
+const newValueSet = (currentValue, id, checked) => {
+  if(checked){
+    return [
+      ...currentValue,
+      id,
+    ];
+  } else {
+    return currentValue.filter(value => value != id);
+  }
+};
+
+const OrderOptionCheckboxes = ({values, currentValue, setOptionValue}) => {
   return (
-    <div>
-
+    <div className={styles.checkboxes}>
+      {values.map(value => (
+        <label key={value.id}>
+          <input
+            type='checkbox'
+            value={value.id}
+            onChange={event => 
+              setOptionValue(
+                newValueSet(currentValue, value.id, event.currentTarget.checked))}
+          >
+          </input>
+          {value.name} {value.price}
+        </label>
+      ))}
     </div>
   );
 };
 
 OrderOptionCheckboxes.propTypes = {
   name: PropTypes.string,
+  price: PropTypes.number,
+  values: PropTypes.array,
+  currentValue: PropTypes.array,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionCheckboxes;
---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+
+const OrderOptionText = ({ setOptionValue, currentValue }) => {
+  return (
+    console.log(currentValue),
+    <div className={styles.text}>
+        
+      <input 
+        type='text'
+        name={currentValue}
+        onChange={event => setOptionValue(event.currentTarget.currentValue)}
+      >
+          
+      </input>
+      
+    </div>
+  );
+};
+
+OrderOptionText.propTypes = {
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.string,
+};
+
+export default OrderOptionText;
